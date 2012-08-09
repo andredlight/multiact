a7ae05bfc2d7ce1a0d6c21db4e1f685b85c756a6
Uwe Kubosch <uwe@kubosch.no>
2012-08-09 10:57:05 +0200

    * Use the new class based activity definition for the main activity.

--- src/multiact_activity.rb
+++ src/multiact_activity.rb
@@ -6,27 +6,23 @@ ruboto_import_widgets :Button, :LinearLayout, :TextView
 
 # http://xkcd.com/378/
 
-class RubotoActivity
-  def self.main(context)
-    context.start_ruboto_activity("$activity_main") do
-      def on_create(bundle)
-        setTitle "main activity"
-        setContentView(
-          linear_layout(:orientation => :vertical) do
-            @text_view = text_view :text => 'What hath Matz wrought?', :id => 42, :width => :fill_parent,
+class MultiactActivity
+  def on_create(bundle)
+    setTitle "main activity"
+    setContentView(
+        linear_layout(:orientation => :vertical) do
+          @text_view = text_view :text => 'What hath Matz wrought?', :id => 42, :width => :fill_parent,
                                  :gravity => android.view.Gravity::CENTER, :text_size => 48.0
-            button :text => "Next activity", :id => 43, :width => :fill_parent, :on_click_listener => @on_click_next
-          end
-        )
-      end
-      @on_click_next = proc do |button|
-        RubotoActivity.next(self)
-      end
-    end
+          button :text => "Next activity", :id => 43, :width => :fill_parent, :on_click_listener => proc { next_activity }
+        end
+    )
   end
 
-  def self.next(context)
-    context.start_ruboto_activity("$activity_next") do
+  private
+
+  def next_activity
+    puts 'Start next activity'
+    start_ruboto_activity("$activity_next") do
       def on_create(bundle)
         setContentView(
           linear_layout(:orientation => :vertical) do
@@ -38,5 +34,3 @@ class RubotoActivity
     end
   end
 end
-
-RubotoActivity.main $activity 
